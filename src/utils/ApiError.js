d796c039ae714235de4a00ae1b79cf0cc5351c4b
starkt887 <starkt887@gmail.com>
2025-06-17 20:57:34 +0530

    init uploader logic

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,8 +1,22 @@
 class ApiError extends Error {
-  constructor(statusCode, message = "Something went wrong!!", data) {
+  constructor(
+    statusCode,
+    message = "Something went wrong!!",
+    data,
+    errors = [],
+    stack = ""
+  ) {
     this.statusCode = statusCode;
     this.message = message;
     this.data = data;
     this.success = statusCode < 400;
+    this.errors = errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
+
+export default ApiError
