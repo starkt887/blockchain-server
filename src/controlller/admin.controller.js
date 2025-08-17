3ca06192f43e64ea8727f21f0df9e6a143b0650b
starkt887 <starkt887@gmail.com>
2025-08-17 09:01:04 +0530

    admin controller

--- src/controlller/admin.controller.js
+++ src/controlller/admin.controller.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "bson";
 import { ROLES, ROWS_LIMIT } from "../constants.js";
 import DB from "../db/index.js";
 import ApiError from "../utils/ApiError.js";
@@ -46,31 +47,62 @@ const getAllCompanies = asyncHandler(async (req, res) => {
 
 const searchUsersByIDorCompanyName = asyncHandler(async (req, res) => {
   const { query, page } = req.query;
+  console.log(req.query);
+
   const roleDetails = await DB.roles.findFirst({
     where: { title: ROLES.USER },
   });
-
   if (!roleDetails) {
     throw new ApiError(401, "No User role found!");
   }
   const skip = (page - 1) * ROWS_LIMIT;
+  if (!query) {
+    const totalUsersMatchingQuery = await DB.user.findMany({
+      where: {
+        roleId: roleDetails.id,
+      },
+      omit: { password: true, refreshToken: true },
+    });
+    const users = await DB.user.findMany({
+      skip: skip,
+      take: ROWS_LIMIT,
+      where: {
+        roleId: roleDetails.id,
+      },
+      omit: { password: true, refreshToken: true },
+    });
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { users, totalUserCount: totalUsersMatchingQuery.length },
+          "Search complete!"
+        )
+      );
+    return;
+  }
+  console.log("ID valid:", ObjectId.isValid(query));
+  let searchConditions = [
+    { company: { startsWith: query, mode: "insensitive" } },
+  ];
+  if (ObjectId.isValid(query)) {
+    searchConditions.push({ id: query });
+  }
+
   const totalUsersMatchingQuery = await DB.user.findMany({
     where: {
-      OR: [
-        { roleId: { equals: query, mode: "insensitive" } },
-        { company: { startsWith: query, mode: "insensitive" } },
-      ],
+      OR: searchConditions,
     },
     omit: { password: true, refreshToken: true },
   });
+  console.log("totalUsers:", totalUsersMatchingQuery.length);
+
   const users = await DB.user.findMany({
-    skip,
+    skip: skip,
     take: ROWS_LIMIT,
     where: {
-      OR: [
-        { roleId: { equals: query, mode: "insensitive" } },
-        { company: { startsWith: query, mode: "insensitive" } },
-      ],
+      OR: searchConditions,
     },
     omit: { password: true, refreshToken: true },
   });
